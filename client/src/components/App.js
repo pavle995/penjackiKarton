7f4d0c48efa3413800b06e2f3245d2a3f5f25182
pkosutic <pkosutic@nanointeractive.com>
2019-07-31 16:27:06 +0200

    First components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Header from './Header'
+
+const WelcomePage = () => <h2>WelcomePage</h2>
 
 const App = () => {
 	return (
 		<div>
-			Hi There!
+			<BrowserRouter>
+				<div>
+					<Header />
+					<Route exact={true} path="/" component={WelcomePage} />
+				</div>
+			</BrowserRouter>
 		</div>
 	)
 }
 
-export default App
+export default App;
